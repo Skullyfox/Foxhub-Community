fcac4e0da4b8470374ceffe3443f2e7eee63235c
Skullyfox <Skullyfox.yt@gmail.com>
2018-09-26 01:03:01 +0200

    Sharing Embed Meta

--- src/server/controllers/www/AppController.js
+++ src/server/controllers/www/AppController.js
@@ -39,6 +39,16 @@ module.exports = async (req, res, next) => {
                     <head>
                         <meta name="viewport" content="width=device-width, initial-scale=1">
                         <meta name="theme-color" content="#ffffff">
+
+                        <meta name="twitter:title" content="| Foxhub Community - Accueil |">
+                        <meta name="twitter:description" content="Page d'accueil du site communautaire Foxhub Community">
+                        <meta name="twitter:image" content="/embed.png">
+
+                        <meta property="og:title" content="| Foxhub Community - Accueil |">
+                        <meta property="og:description" content="Page d'accueil du site communautaire Foxhub Community">
+                        <meta property="og:image" content="/embed.png">
+                        <meta name="twitter:card" content="summary_large_image">
+
                         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
                         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
                         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
